0ba6a8166b72013e054d0c04d84234ae533fce25
LeonardoJr96 <leo70x7000@gmail.com>
2023-10-03 18:27:20 -0300

    content update

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -13,14 +13,14 @@ export const dataFake = [
   },
   {
     "id":"3",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "Marvel anúncia um novo filme do Homem de Ferro, confira",
-    "photoCover":"https://th.bing.com/th/id/OIP.0flS2ZP6BT4WxWWsiAemCgHaER?pid=ImgDet&rs=1"
+    "title": "Novo filme do Pantera Negra lançado em breve",
+    "description": "Confira",
+    "photoCover":"https://th.bing.com/th/id/R.849c9fa7e94a6ec54fc97585934ce445?rik=cubmuCklN7gYfg&pid=ImgRaw&r=0"
   },
   {
     "id":"4",
-    "title": "Nova Série anúnciada da Disney +",
+    "title": "Novo filme dos x-men está em pós produção, afirma diretor",
     "description": "Confira",
-    "photoCover":"https://th.bing.com/th/id/OIP.P9O5u5p6umcSrUSHFo7Z8gHaE5?pid=ImgDet&rs=1"
+    "photoCover":"https://th.bing.com/th/id/OIP.J6zZq1iWTFUlIN2A17aMWQHaEo?w=186&h=116&c=7&r=0&o=5&pid=1.7"
   }
 ]
