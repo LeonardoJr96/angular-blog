179c3277679a44173fbc6cf78d1712a421dec51c
LeonardoJr96 <leo70x7000@gmail.com>
2023-10-03 18:23:41 -0300

    add dinamycs page

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
   {
-    "id":1,
-    "photo":"aaa",
-    "title": "saiu novo filme",
-    "description": "ola"
+    "id":"1",
+    "title": "NOVO HOMEM DE FERRO EM 3D",
+    "description": "Marvel anúncia um novo filme do Homem de Ferro, confira",
+    "photoCover":"https://th.bing.com/th/id/OIP.0flS2ZP6BT4WxWWsiAemCgHaER?pid=ImgDet&rs=1"
   },
   {
-    "id":1,
-    "photo":"aaa",
-    "title": "saiu novo filme",
-    "description": "ola"
+    "id":"2",
+    "title": "Nova Série anúnciada da Disney +",
+    "description": "Confira",
+    "photoCover":"https://th.bing.com/th/id/OIP.P9O5u5p6umcSrUSHFo7Z8gHaE5?pid=ImgDet&rs=1"
+  },
+  {
+    "id":"3",
+    "title": "NOVO HOMEM DE FERRO EM 3D",
+    "description": "Marvel anúncia um novo filme do Homem de Ferro, confira",
+    "photoCover":"https://th.bing.com/th/id/OIP.0flS2ZP6BT4WxWWsiAemCgHaER?pid=ImgDet&rs=1"
+  },
+  {
+    "id":"4",
+    "title": "Nova Série anúnciada da Disney +",
+    "description": "Confira",
+    "photoCover":"https://th.bing.com/th/id/OIP.P9O5u5p6umcSrUSHFo7Z8gHaE5?pid=ImgDet&rs=1"
   }
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { dataFake } from '../../data/dataFake';
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-  photoCover:string ="https://th.bing.com/th/id/OIP.0flS2ZP6BT4WxWWsiAemCgHaER?pid=ImgDet&rs=1"
-  contentTitle:string ="NOTICIA EXEMPLO"
-  contentDescription:string ="bla bla bla"
+  photoCover:string =""
+  contentTitle:string =""
+  contentDescription:string =""
+  private id:string | null ="0"
 
   constructor(
     private ROUTE:ActivatedRoute
@@ -17,8 +18,19 @@ export class ContentComponent implements OnInit {
 
   ngOnInit(): void {
     this.ROUTE.paramMap.subscribe( value =>
-      console.log(value.get("id"))
+      this.id = value.get("id")
     )
+
+    this.setValuesToComponent(this.id)
   }
+
+  setValuesToComponent(id:string | null){
+    const result = dataFake.filter(article => article.id == this.id)[0]
+
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photoCover
+    
+  } 
 
 }
